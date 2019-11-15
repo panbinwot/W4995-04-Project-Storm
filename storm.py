2d0cf636cdcca2592c5ca3122f4ccad81242b5b3
Bin Pan <binpan@dyn-160-39-195-33.dyn.columbia.edu>
2019-11-14 23:31:01 -0500

    Thursday Update

--- storm.py
+++ storm.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 '''
 Pytorch implementation of Stochastic Recursive Momentum 
 
@@ -10,9 +12,52 @@ import torch
 from torch.optim import Optimizer
 
 class Storm(Optimizer):
-    def __init__(self):
-        pass
+    def __init__(self, params, lr = 0.001, momentum = 0, dampening = 0, weight_decay = 0, nesterov = False ):
+        if lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if momentum < 0.0:
+            raise ValueError("Invalid momentum value: {}".format(momentum))
+        if weight_decay < 0.0:
+            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+
+        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
+                        weight_decay=weight_decay, nesterov=nesterov)
+
+        if nesterov and (momentum <= 0 or dampening != 0):
+            raise ValueError("Nesterov momentum requires a momentum and zero dampening")
+        super(SGD, self).__init__(params, defaults)
+
     def __setstate__(self,state):
         super(Storm, self).__setstate__(state)
-        loss = None
+        for group in self.add_param_groups:
+            group.setdefault("nesterov",False)
+
+        def step(self, closure = None):
+
+            loss = None
+            if closure is not None:
+                loss = closure()
+            for group in self.add_param_groups:
+                weight_decay =  group['weight_decay']
+                momentum = group['momentum']
+                dampening = group['dampening']
+                nesterov = group['nesterov']
+
+                for  p in group['params']:
+                    if p.grad is None: continue
+                    d_p = p.grad.data 
+                    if weight_decay !=0:
+                        d_p.add_(weight_decay, p.data)
+                    if momentum != 0:
+                        param_state = self.state[p]
+                        if 'momentum_buffer' not in param_state:
+                            buf = param_state['momentum_buffer'] = torch.clone.(d_p).detach()
+                        else:
+                            buf = param_state['momentum_buffer']
+                            buf.mul_(momentum).add_(1 - dampening, d_p)
+                        if nesterov:
+                            d_p = d_p.add(momentum, buf)
+                        else:
+                            d_p = buf
+                    p.data.add_(-group['lr'],d_p)
         return loss
---
+++ test.py
@@ -0,0 +1,124 @@
+import os
+import numpy as np
+import _pickle as pickle
+import matplotlib.pyplot as plt
+
+import torch
+import torchvision
+import torch.nn as nn
+import torchvision.models as models
+
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+
+# the followings are self-defined
+import LoadBatch
+from helper import *
+
+def train_test(trainloader, testloader, net, optimizer, criterion, epochs):
+  train_loss_lst = []
+  train_acc_lst = []
+  test_acc_lst = []
+  i = 0
+  for epoch in range(epochs):
+    
+    for i,data in enumerate(trainloader, 0):
+      net.train()
+      batch_x = data[0].cuda()
+      batch_y = data[1].cuda()
+      optimizer.zero_grad()
+      output = net(batch_x)
+      loss = criterion(output, batch_y)
+      train_loss_lst.append(loss)
+      loss.backward()
+      optimizer.step()
+
+      # Evaluate at this batch
+
+      net.eval()
+      with torch.no_grad():
+        optimizer.zero_grad()
+        outputs = net(batch_x)
+        _, predicted = torch.max(outputs.data, 1)
+
+        train_pred_batch, train_target_batch, = predicted.cpu().detach().numpy(),data[1].cpu().detach().numpy()
+
+        train_acc = np.mean(train_pred_batch.astype(np.int)==train_target_batch )
+        train_acc_lst.append(train_acc)
+
+      # Test the function on the set
+      test_acc = test(testloader, net, optimizer, criterion)
+      test_acc_lst.append( test_acc)
+      
+
+      print("Epoch: {}, Batch: {}, Train Accuracy: {}, Test Accuracy: {}".format(epoch+1, i+1, round(train_acc,3), round(test_acc,3)))
+
+      i += 1
+
+  return train_loss_lst, train_acc_lst, test_acc_lst
+
+def test(testloader, net, optimizer, criterion):
+  test_pred, test_target = [], []
+  net.eval()
+  with torch.no_grad():
+    for data in testloader:
+      
+      batch_x =  data[0].cuda()
+      optimizer.zero_grad()
+      outputs = net(batch_x)
+      _, predicted = torch.max(outputs.data, 1)
+
+      test_pred.append( predicted.cpu().detach().numpy() )
+      test_target.append(data[1].cpu().detach().numpy())
+    test_pred = np.concatenate(test_pred).ravel()
+    test_target = np.concatenate(test_target).ravel()
+  return np.mean(test_pred.astype(np.int)==test_target ) 
+
+def main():
+    if not os.path.exists('./data/'):
+        wget_cmd = 'wget https://s3-eu-west-1.amazonaws.com/bsopenbucket/e6040/data.zip'
+        unzip_cmd = 'unzip data.zip'
+        os.system(wget_cmd)
+        os.system(unzip_cmd)
+    images, labels, mean, std = read_data('data/cifar10')
+    
+    batch_size = 64
+
+    data_train = LoadBatch(np.moveaxis(images['train'], 3, 1), labels['train'])
+    data_test = LoadBatch(np.moveaxis(images['test'], 3, 1), labels['test'])
+    trainloader = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=2)
+    testloader = DataLoader(data_test, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    net = models.resnet34(pretrained=False).cuda()
+
+    adam_optim = torch.optim.Adam(net.parameters())
+    adagrad_optim = torch.optim.Adagrad(net.parameters())
+    criterion = nn.CrossEntropyLoss().cuda()
+    res_Adam = train_test(trainloader, testloader, net, adam_optim, criterion, epochs = 1)
+    res_Adagrad = train_test(trainloader, testloader ,net, adagrad_optim, criterion, epochs = 1)
+
+
+    plt.plot(res_Adam[0], label = "Adam")
+    plt.plot(res_Adagrad[0], label = "Adagrad")
+    plt.legend()
+    plt.xlabel("Steps")
+    plt.ylabel("Loss")
+    plt.savefig('./image_output/1.png')
+
+    plt.plot(res_Adam[1], label = "Adam")
+    plt.plot(res_Adagrad[1], label = "Adagrad")
+    plt.legend()
+    plt.xlabel("Steps")
+    plt.ylabel("Accuracy")
+    plt.savefig('./image_output/2.png')
+
+    plt.plot(res_Adam[2], label = "Adam")
+    plt.plot(res_Adagrad[2], label = "Adagrad")
+    plt.legend()
+    plt.xlabel("Steps")
+    plt.ylabel("Accuracy")
+    plt.savefig('./image_output/3.png')
+
+if __name__ == "__main__":
+    main()
