2297fe0b901992ea0ccb2521ab83ef3b977f5933
PangBing <39650126+panbinwot@users.noreply.github.com>
2019-11-16 13:52:29 -0500

    fix

--- LoadBatch.py
+++ LoadBatch.py
@@ -2,16 +2,15 @@ from torch.autograd import Variable
 from torch.utils.data import Dataset
 
 class LoadBatch(Dataset):
-  """dataset class for dataloader"""
-  def __init__(self, X, y):
-    self.X = X
-    self.y = y
+	def __init__(self, X, y):
+		self.X = X
+		self.y = y
   
-  def __len__(self):
-    dataset_size = len(self.X)
-    return dataset_size
+	def __len__(self):
+		dataset_size = len(self.X)
+		return dataset_size
   
-  def __getitem__(self, idx):
-    X_batch = self.X[idx]
-    y_batch = self.y[idx]
-    return X_batch, y_batch
+	def __getitem__(self, idx):
+		X_batch = self.X[idx]
+		y_batch = self.y[idx]
+		return X_batch, y_batch
--- test.py
+++ test.py
@@ -17,107 +17,107 @@ from LoadBatch import LoadBatch
 from helper import *
 
 def train_test(trainloader, testloader, net, optimizer, criterion, epochs):
-  train_loss_lst = []
-  train_acc_lst = []
-  test_acc_lst = []
-  i = 0
-  for epoch in range(epochs):
-    
-    for i,data in enumerate(trainloader, 0):
-        net.train()s
-        batch_x = data[0]
-        batch_y = data[1]
-        optimizer.zero_grad()
-        output = net(batch_x)
-        loss = criterion(output, batch_y.long())
-        train_loss_lst.append(loss)
-        loss.backward()
-        optimizer.step()
+	train_loss_lst = []
+	train_acc_lst = []
+	test_acc_lst = []
+	test_acc = 0
 
-      # Evaluate at this batch
+	for epoch in range(epochs):
+		i = 0
+		for data in trainloader:
+			net.train()
+			batch_x = data[0]
+			batch_y = data[1]
+			optimizer.zero_grad()
+			output = net(batch_x)
+			loss = criterion(output, batch_y.long())
+			train_loss_lst.append(loss)
+			loss.backward()
+			optimizer.step()
 
-        net.eval()
-        with torch.no_grad():
-            optimizer.zero_grad()
-            outputs = net(batch_x)
-            _ , predicted = torch.max(outputs.data, 1)
+		# Evaluate at this batch
 
-            train_pred_batch, train_target_batch, = predicted.cpu().detach().numpy(),data[1].cpu().detach().numpy()
+			net.eval()
+			with torch.no_grad():
+				optimizer.zero_grad()
+				outputs = net(batch_x)
+				_ , predicted = torch.max(outputs.data, 1)
 
-            train_acc = np.mean(train_pred_batch.astype(np.int)==train_target_batch )
-            train_acc_lst.append(train_acc)
+				train_pred_batch, train_target_batch, = predicted.cpu().detach().numpy(),data[1].cpu().detach().numpy()
 
-      # Test the function on the set
-        test_acc = test(testloader, net, optimizer, criterion)
-        test_acc_lst.append( test_acc)
-    
-        print("Epoch: {}, Batch: {}, Train Accuracy: {}, Test Accuracy: {}".format(epoch+1, i+1, round(train_acc,3), round(test_acc,3)))
+				train_acc = np.mean(train_pred_batch.astype(np.int)==train_target_batch )
+				train_acc_lst.append(train_acc)
 
-        i += 1
+		# Test the function on the set
+			# test_acc = test(testloader, net, optimizer, criterion)
+			# test_acc_lst.append( test_acc)
+	
+			print("Epoch: {}, Batch: {}, Train Accuracy: {}, Test Accuracy: {}".format(epoch+1, i+1, round(train_acc,3), round(test_acc,3)))
 
-    return train_loss_lst, train_acc_lst, test_acc_lst
+			i += 1
+
+	return train_loss_lst, train_acc_lst, test_acc_lst
 
 def test(testloader, net, optimizer, criterion):
-    test_pred, test_target = [], []
-    net.eval()
-    with torch.no_grad():
-        for data in testloader:
-      
-        batch_x =  data[0]
-        optimizer.zero_grad()
-        outputs = net(batch_x)
-        _, predicted = torch.max(outputs.data, 1)
+	test_pred, test_target = [], []
+	net.eval()
+	with torch.no_grad():
+		for data in testloader:
+			batch_x =  data[0]
+			optimizer.zero_grad()
+			outputs = net(batch_x)
+			_ , predicted = torch.max(outputs.data, 1)
 
-        test_pred.append( predicted.cpu().detach().numpy() )
-        test_target.append(data[1].cpu().detach().numpy())
-    test_pred = np.concatenate(test_pred).ravel()
-    test_target = np.concatenate(test_target).ravel()
-    return np.mean(test_pred.astype(np.int)==test_target ) 
+			test_pred.append( predicted.cpu().detach().numpy() )
+			test_target.append(data[1].cpu().detach().numpy())
+		test_pred = np.concatenate(test_pred).ravel()
+		test_target = np.concatenate(test_target).ravel()
+	return np.mean(test_pred.astype(np.int)==test_target ) 
 
 def main():
-    if not os.path.exists('./data/'):
-        wget_cmd = 'wget https://s3-eu-west-1.amazonaws.com/bsopenbucket/e6040/data.zip'
-        unzip_cmd = 'unzip data.zip'
-        os.system(wget_cmd)
-        os.system(unzip_cmd)
-    images, labels, mean, std = read_data('data/')
-    
-    batch_size = 64
+	if not os.path.exists('./data/'):
+		wget_cmd = 'wget https://s3-eu-west-1.amazonaws.com/bsopenbucket/e6040/data.zip'
+		unzip_cmd = 'unzip data.zip'
+		os.system(wget_cmd)
+		os.system(unzip_cmd)
+	images, labels, mean, std = read_data('data/')
+	
+	batch_size = 64
 
-    data_train = LoadBatch(np.moveaxis(images['train'], 3, 1), labels['train'])
-    data_test = LoadBatch(np.moveaxis(images['test'], 3, 1), labels['test'])
-    trainloader = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=2)
-    testloader = DataLoader(data_test, batch_size=batch_size, shuffle=False, num_workers=2)
+	data_train = LoadBatch(np.moveaxis(images['train'], 3, 1), labels['train'])
+	data_test = LoadBatch(np.moveaxis(images['test'], 3, 1), labels['test'])
+	trainloader = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=2)
+	testloader = DataLoader(data_test, batch_size=batch_size, shuffle=False, num_workers=2)
 
-    net = models.resnet34(pretrained=False)
+	net = models.resnet34(pretrained=False)
+	net2 = models.resnet34(pretrained=False)
+	adam_optim = torch.optim.Adam(net.parameters())
+	adagrad_optim = torch.optim.Adagrad(net2.parameters())
+	criterion = nn.CrossEntropyLoss()
+	res_Adam = train_test(trainloader, testloader, net, adam_optim, criterion, epochs = 1)
+	res_Adagrad = train_test(trainloader, testloader ,net2, adagrad_optim, criterion, epochs = 1)
 
-    adam_optim = torch.optim.Adam(net.parameters())
-    adagrad_optim = torch.optim.Adagrad(net.parameters())
-    criterion = nn.CrossEntropyLoss()
-    res_Adam = train_test(trainloader, testloader, net, adam_optim, criterion, epochs = 1)
-    res_Adagrad = train_test(trainloader, testloader ,net, adagrad_optim, criterion, epochs = 1)
+	print("working")
+	plt.plot(res_Adam[0], label = "Adam")
+	plt.plot(res_Adagrad[0], label = "Adagrad")
+	plt.legend()
+	plt.xlabel("Steps")
+	plt.ylabel("Loss")
+	plt.savefig('./image_output/1.png')
 
-    print("working")
-    plt.plot(res_Adam[0], label = "Adam")
-    plt.plot(res_Adagrad[0], label = "Adagrad")
-    plt.legend()
-    plt.xlabel("Steps")
-    plt.ylabel("Loss")
-    plt.savefig('./image_output/1.png')
+	plt.plot(res_Adam[1], label = "Adam")
+	plt.plot(res_Adagrad[1], label = "Adagrad")
+	plt.legend()
+	plt.xlabel("Steps")
+	plt.ylabel("Accuracy")
+	plt.savefig('./image_output/2.png')
 
-    plt.plot(res_Adam[1], label = "Adam")
-    plt.plot(res_Adagrad[1], label = "Adagrad")
-    plt.legend()
-    plt.xlabel("Steps")
-    plt.ylabel("Accuracy")
-    plt.savefig('./image_output/2.png')
-
-    plt.plot(res_Adam[2], label = "Adam")
-    plt.plot(res_Adagrad[2], label = "Adagrad")
-    plt.legend()
-    plt.xlabel("Steps")
-    plt.ylabel("Accuracy")
-    plt.savefig('./image_output/3.png')
+	plt.plot(res_Adam[2], label = "Adam")
+	plt.plot(res_Adagrad[2], label = "Adagrad")
+	plt.legend()
+	plt.xlabel("Steps")
+	plt.ylabel("Accuracy")
+	plt.savefig('./image_output/3.png')
 
 if __name__ == "__main__":
-    main()
+	main()
